57e97789c36371ecbfb96c3e0004555abd7f66df
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-10-15 23:45:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/parent-profile" element={<ParentProfile />} />
         <Route path="/child-profile" element={<ChildProfile />} />
         <Route path="/Ai-Chat" element={<ChatAi />} />
+<Route path="/hello" element={console.log(”hello")} />
       </Routes>
     </Router>
   );
