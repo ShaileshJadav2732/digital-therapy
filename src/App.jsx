a73ddbadc0ed321eed8497e85cee66d040d204d7
Shailesh <jshailesh798@gmail.com>
2025-02-06 13:47:53 +0530

    add bug_fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import ParentProfile from './components/ParentProfile';
-import ChildProfile from './components/ChildProfile';
-import ChatAi from './components/ChatAi'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import ParentProfile from "./components/ParentProfile";
+import ChildProfile from "./components/ChildProfile";
+import ChatAi from "./components/ChatAi";
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/parent-profile" element={<ParentProfile />} />
-        <Route path="/child-profile" element={<ChildProfile />} />
-        <Route path="/Ai-Chat" element={<ChatAi />} />
-<Route path="/hello" element={console.log("hello")} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/parent-profile" element={<ParentProfile />} />
+				<Route path="/child-profile" element={<ChildProfile />} />
+				<Route path="/Ai-Chat" element={<ChatAi />} />
+				<Route path="/hello" element={console.log("hello")} />
+				<p>hello world</p>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
