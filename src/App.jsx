e2c1aaaaa499d86e65fa5e0f9a5a2c8fb2aac660
Shailesh Jadav <113022776+ShaileshJadav2732@users.noreply.github.com>
2025-02-06 16:00:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 				<Route path="/Ai-Chat" element={<ChatAi />} />
 				<Route path="/hello" element={console.log("hello")} />
 				<p>hello world2</p>
+				<h1>hello world 3</h1>
 			</Routes>
 		</Router>
 	);
